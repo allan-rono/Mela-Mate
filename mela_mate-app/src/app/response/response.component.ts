56929bce02b37927130796aebd22ce9662f45311
Allan Rono <58682072+allan-rono@users.noreply.github.com>
2023-03-28 09:14:05 +1100

    Frontend Component Code added

--- mela_mate-app/src/app/response/response.component.ts
+++ mela_mate-app/src/app/response/response.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-response',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./response.component.css']
 })
 export class ResponseComponent {
+  response = '';
 
+  constructor(private http: HttpClient) {}
+
+  onSubmit() {
+    this.http.get('http://localhost:4200/response')
+      .subscribe((response: any) => {
+        this.response = response;
+      });
+  }
 }
--- mela_mate-app/src/app/upload-image/upload-image.component.ts
+++ mela_mate-app/src/app/upload-image/upload-image.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-upload-image',
@@ -6,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./upload-image.component.css']
 })
 export class UploadImageComponent {
+  selectedFile: File | null = null;
 
+  constructor(private http: HttpClient) {}
+
+  onFileSelected(event: any) {
+    this.selectedFile = event.target.files[0];
+  }
+
+  onSubmit() {
+    if (!this.selectedFile) {
+      console.error('No file selected.');
+      return;
+    }
+    const fd = new FormData();
+    fd.append('image', this.selectedFile, this.selectedFile.name);
+    this.http.post('http://localhost:4200/upload', fd)
+      .subscribe((response: any) => {
+        console.log(response);
+      });
+  }
 }
