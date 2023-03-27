4a6e3c5ae9372cb36590b92efa107a66c3ce56e5
Allan Rono <58682072+allan-rono@users.noreply.github.com>
2023-03-28 09:27:01 +1100

    Server js file implemented to handle uploaded jpeg

--- server.js
+++ server.js
@@ -1,6 +1,35 @@
 const express = require('express');
 const multer = require('multer');
-const { exec } = require('child_process');
+const { spawn } = require('child_process');
 
 const app = express();
-const port = 3000;
+
+// Set up multer for file uploads
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploads');
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + '-' + file.originalname);
+  }
+});
+
+const upload = multer({ storage: storage });
+
+// Define server endpoints
+app.post('/upload', upload.single('image'), (req, res) => {
+  // Get file path of uploaded image
+  const imagePath = req.file.path;
+
+  // Run Python script to determine if image is melanoma
+  const pythonProcess = spawn('python', ['melanoma_detection.py', imagePath]);
+  pythonProcess.stdout.on('data', (data) => {
+    console.log(`stdout: ${data}`);
+    // Send response to client
+    res.send(data);
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
